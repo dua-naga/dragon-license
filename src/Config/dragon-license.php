4a061a67e88d6a18df9246e42509a42e2b8f8ce5
SyafiqMSI <muhammadsyafiqibra@gmail.com>
2026-01-14 08:36:07 +0700

    fix: update server URL for licensing configuration

--- src/Config/dragon-license.php
+++ src/Config/dragon-license.php
@@ -10,7 +10,7 @@ return [
     | You can override this in your .env file with DRAGON_LICENSE_URL
     |
     */
-    'server_url' => 'http://localhost:8000',
+    'server_url' => 'https://licensing.nagaverse.id',
 
     /*
     |--------------------------------------------------------------------------
