025a32f3dfe1a51f755b4f6f4b14cb5e9fe33308
SyafiqMSI <muhammadsyafiqibra@gmail.com>
2026-02-10 15:08:08 +0700

    chore: update license server URL in configuration.

--- src/Config/dragon-license.php
+++ src/Config/dragon-license.php
@@ -10,7 +10,7 @@ return [
     | You can override this in your .env file with DRAGON_LICENSE_URL
     |
     */
-    'server_url' => 'https://licensing.nagaverse.id',
+    'server_url' => 'https://licensing.duanaga.id',
 
     /*
     |--------------------------------------------------------------------------
